19e21952f12d9b38320781e9619a36ed9ab7bd04
dkcpllmd <dukcapil.lamandau@gmail.com>
2025-06-19 14:41:58 +0700

    update

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -9,10 +9,14 @@
     </div>
 </footer>
 
-    </div>
+</div>
 
-    <!-- JS CDN Bootstrap 5 -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+<!-- JS CDN Bootstrap 5 -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script> -->
+
+<script src="<?= base_url('../dist/assets/js/popper.min.js') ?>"></script>
+<script src="<?= base_url('../dist/assets/js/bootstrap.bundle.min.js') ?>"></script>
 </body>
+
 </html>
--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -6,10 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $title ?> | Antrian Dukcapil Kab. Lamandau</title>
 
-    <!-- Link CDN Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= base_url('../dist/assets/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('../dist/assets/fonts/Roboto.css') ?>">
 
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Poppins:wght@400;600&display=swap"
+        rel="stylesheet"> -->
 </head>
 
 <body>
