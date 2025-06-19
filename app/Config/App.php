a1b03615e1bb1121723f3827d46a4d316c6c7115
dkcpllmd <dukcapil.lamandau@gmail.com>
2025-06-19 17:10:56 +0700

    update

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://192.168.1.5:8080/';
+    public string $baseURL = 'http://localhost:8080/';
 
 
     /**
@@ -41,7 +41,7 @@ class App extends BaseConfig
      * something else. If you have configured your web server to remove this file
      * from your site URIs, set this variable to an empty string.
      */
-    public string $indexPage = 'index.php';
+    public string $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
